5200ba3b2325e1caa47785e653de123cdc9edc4a
Kostya Gorbunov <kgorbunov@viber.com>
2019-07-12 14:21:58 +0200

    - initial project commit;

---
+++ FashionCloud/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FashionCloud
+//
+//  Created by Kostiantyn Gorbunov on 12/07/2019.
+//  Copyright © 2019 Kostiantyn Gorbunov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
