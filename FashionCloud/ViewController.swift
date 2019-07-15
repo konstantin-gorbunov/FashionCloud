8c59f5f4bb939001b6f975455067bd90729bd202
Kostya Gorbunov <kgorbunov@viber.com>
2019-07-15 09:48:51 +0200

    - add stream based CSV parser for catalog parsing;

--- FashionCloud/ViewController.swift
+++ FashionCloud/ViewController.swift
@@ -9,6 +9,12 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    private enum Parser: Error {
+        case noSourceFile
+        case mapperNotFound
+        case parsingProblem
+    }
 
     private var mapper: Mapper?
     
@@ -24,20 +30,59 @@ class ViewController: NSViewController {
     // MARK: - IBActions - menus
     
     @IBAction func startParsing(_ sender: Any) {
-        produceParsing()
+        do {
+            try produceParsing()
+        } catch {
+            print("\(error)")
+        }
     }
 
     // MARK: - private
     
-    private func produceParsing() {
+    private func produceParsing() throws {
         guard let pricatPath = pricatPath else {
             print("Can't find pricat CSV file.")
+            throw Parser.noSourceFile
+        }
+        guard let _ = mapper else {
+            print("Mapper is not initialized.")
+            throw Parser.mapperNotFound
+        }
+        let pricatURL = NSURL.fileURL(withPath: pricatPath)
+
+        guard let stream = InputStream(url: pricatURL) else {
             return
         }
-        let pricatRequest = URLRequest(url: NSURL.fileURL(withPath: pricatPath))
-        
-        print("produceParsing of \(pricatRequest) with \(mapper)")
-        // TODO: produceParsing
+        let configuration = CSV.Configuration(delimiter: ";", encoding: .utf8)
+
+        let parser = CSV.Parser(inputStream: stream, configuration: configuration)
+        parser.delegate = self
+        do {
+            try parser.parse()
+        } catch {
+            throw Parser.parsingProblem
+        }
     }    
 }
 
+extension ViewController: ParserDelegate {
+    func parserDidBeginDocument(_ parser: CSV.Parser) {
+        print("DidBeginDocument")
+    }
+    
+    func parserDidEndDocument(_ parser: CSV.Parser) {
+        print("DidEndDocument")
+    }
+    
+    func parser(_ parser: CSV.Parser, didBeginLineAt index: UInt) {
+        
+    }
+    
+    func parser(_ parser: CSV.Parser, didEndLineAt index: UInt) {
+        print("DidEndLine \(index)")
+    }
+    
+    func parser(_ parser: CSV.Parser, didReadFieldAt index: UInt, value: String) {
+        
+    }
+}
